d2fe76e0a1b8bc30b3085dd67ed33dd9cdab1329
Dharmik Batra <batradharmik@gmail.com>
2021-07-03 16:21:10 +0530

    lts version

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -8,21 +8,35 @@ import { CardData } from './card/carddata.model';
 })
 export class SubscriptionComponent implements OnInit {
 
-  cardData:CardData[]=[
-    new CardData("hello service","qwsadssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssser","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd"),
-    new CardData("hello service","qwer","weqw","dwdqw","dqwd")
+  cardDataMain:CardData[]=[
+    new CardData("hello1 service","qwsssssssssssssssssssssssssssssssssssser","weqw","dwdqw","dqwd"),
+    new CardData("hello2 service","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello3 service","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello4 service","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello5 service","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello6 service","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello7","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello8 service","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello9 service","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello10 service","qwer","weqw","dwdqw","dqwd"),
+    new CardData("hello11 service","qwer","weqw","dwdqw","dqwd")
   ];
+  cardData:CardData[]=[];
   ngOnInit(): void {
   }
-  constructor() { }
+  constructor() { 
+    this.cardData=this.cardDataMain;
+  }
+  
+
+  onsearch(searcheddata:any){
+      
+      // console.log(this.cardDataMain[1].serviceName);
+      var n=this.cardDataMain.length;
+      this.cardData=[];
+      for (let i = 0; i < n; i++) {
+          if(this.cardDataMain[i].serviceName===searcheddata.serviceName){this.cardData.push(this.cardDataMain[i]);}
+      }
+  }
 
 }
